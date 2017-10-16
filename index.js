f9be71c3cfd007c84c9cf3606bb59259ed252b0f
Liam Butterworth <liam.butterworth@ua.edu>
2017-10-16 11:53:23 -0500

    make sure to return value segment if no bassline function exists

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 var postcss        = require( 'postcss' );
 var VerticalRhythm = require( './vertical-rhythm' );
 
-var defaults = {
-	baseFontSize:   '16px',
-	baseLineHeight: '1.5',
-};
-
 module.exports = postcss.plugin( 'bassline', function( options ) {
 
 	// iterate over the object and get any property name that's a function
@@ -27,6 +22,11 @@ module.exports = postcss.plugin( 'bassline', function( options ) {
 	}
 
 	return function( css ) {
+		var defaults = {
+			baseFontSize:   '16px',
+			baseLineHeight: '1.5',
+		};
+
 		options = Object.assign( {}, defaults, options );
 		var verticalRhythm = new VerticalRhythm( options );
 
@@ -52,18 +52,17 @@ module.exports = postcss.plugin( 'bassline', function( options ) {
 					// iterate over the value segments and compile output for bassline methods
 					var compiledSegments = valueSegments.map( function( segment ) {
 
-						// test value segment for bassline method
-						if ( regexTest.test( segment ) ) {
+						// if segment doesnt contain a bassline method, pass it back as is
+						if ( !regexTest.test( segment ) ) return segment;
 
-							// get the method name from segment, e.g. "lines" in "lines( 16, 1px )"
-							var method = verticalRhythm[ segment.substr( 0, segment.indexOf( '(' ) ) ];
+						// get the method name from segment, e.g. "lines" in "lines( 16, 1px )"
+						var method = verticalRhythm[ segment.substr( 0, segment.indexOf( '(' ) ) ];
 
-							// get array of parameters from the segment, e.g. ['16', '1px'] in "lines( 16, 1px )"
-							var params = getParams( segment );
+						// get array of parameters from the segment, e.g. ['16', '1px'] in "lines( 16, 1px )"
+						var params = getParams( segment );
 
-							// call the method using the parameter array; ensure 'this' is it's parent object
-							return method.apply( verticalRhythm, params );
-						}
+						// call the method using the parameter array; ensure 'this' is it's parent object
+						return method.apply( verticalRhythm, params );
 					} );
 
 					// define new declaration value by joining the compiled segments
