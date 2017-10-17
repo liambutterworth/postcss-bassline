ba42f6799eb1817281a7058acabb01daec48fa03
Liam Butterworth <liam.butterworth@ua.edu>
2017-10-17 15:37:07 -0500

    add a rounding function to control plugin output

--- vertical-rhythm.js
+++ vertical-rhythm.js
@@ -3,20 +3,30 @@ var VerticalRhythm = function( options ) {
 	this.baseLineHeight = parseInt( options.baseLineHeight, 10 );
 };
 
+VerticalRhythm.prototype.round = function( number ) {
+	return Math.round( number * 10000 ) / 10000;
+}
+
 VerticalRhythm.prototype['font-size'] = function( targetPx ) {
-	var targetFontSize = parseInt( targetPx, 10 );
-	return `${ targetFontSize / this.baseFontSize }rem`;
+	var targetFontSize   = parseInt( targetPx, 10 );
+	var computedFontSize = targetFontSize / this.baseFontSize;
+
+	return `${ this.round( computedFontSize ) }rem`;
 };
 
 VerticalRhythm.prototype['line-height'] = function( targetPx ) {
-	var targetFontSize = parseInt( targetPx, 10 );
-	var lineMultiplier = Math.ceil( targetFontSize / this.baseFontSize );
-	return ( ( this.baseLineHeight * lineMultiplier ) * this.baseFontSize ) / targetFontSize;
+	var targetFontSize     = parseInt( targetPx, 10 );
+	var lineMultiplier     = Math.ceil( targetFontSize / this.baseFontSize );
+	var computedLineHeight = ( ( this.baseLineHeight * lineMultiplier ) * this.baseFontSize ) / targetFontSize;
+
+	return this.round( computedLineHeight );
 };
 
 VerticalRhythm.prototype.lines = function( count, subtractPx ) {
 	var pixelsToSubtract = subtractPx ? ( parseInt( subtractPx, 10 ) / this.baseFontSize ) : 0;
-	return `${ ( count * ( ( this.baseFontSize * this.baseLineHeight ) / this.baseFontSize ) ) - pixelsToSubtract }rem`;
+	var computedValue    = ( count * ( ( this.baseFontSize * this.baseLineHeight ) / this.baseFontSize ) ) - pixelsToSubtract;
+
+	return `${ this.round( computedValue ) }rem`;
 };
 
 module.exports = VerticalRhythm;
