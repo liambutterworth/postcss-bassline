8ec5447bc6a336ff6cebcd2c9df18a0a402055dd
Liam Butterworth <liam.butterworth@ua.edu>
2017-10-18 15:55:14 -0500

    expand test cases with more complex examples

--- rhythm.spec.js
+++ rhythm.spec.js
@@ -1,5 +1,5 @@
 const assert = require( 'chai' ).assert;
-const Rhythm = require( './rhythm.js' );
+const Rhythm = require( './rhythm' );
 
 const rhythm = new Rhythm({
 	baseFontSize:   '16',
@@ -8,7 +8,12 @@ const rhythm = new Rhythm({
 
 describe( 'Rhythm', function() {
 	describe( 'round()', function() {
-		it ( 'should return a number that is no longer than four decimal places', function() {
+		it( 'should return a number', function() {
+			const value = rhythm.round( 0.1234 );
+			assert.typeOf( value, 'number' );
+		} );
+
+		it( 'should return a number that is no longer than four decimal places', function() {
 			const value = rhythm.round( 0.123456789 ).toString().split( '.' ).pop();
 			assert.isAtMost( value.length, 4 );
 		} );
@@ -19,9 +24,24 @@ describe( 'Rhythm', function() {
 			const value = rhythm['font-size']( '16px' );
 			assert.typeOf( value, 'string' );
 		} );
+
+		it( 'should return the correct value', function() {
+			const value = rhythm['font-size']( '16px' );
+			assert.equal( value, '1rem' );
+		} );
+
+		it( 'should be appended with the rem unit', function() {
+			const value = rhythm['font-size']( '16px' );
+			assert.match( value, /rem$/ );
+		} );
 	} );
 
 	describe( 'line-height()', function() {
+		it( 'should return the correct value', function() {
+			const value = rhythm['line-height']( '16px' );
+			assert.equal( value, 1 );
+		} );
+
 		it( 'should return a number', function() {
 			const value = rhythm['line-height']( '16px' );
 			assert.typeOf( value, 'number' );
@@ -29,9 +49,26 @@ describe( 'Rhythm', function() {
 	} );
 
 	describe( 'lines()', function() {
-		it ( 'should return a string', function() {
+		it( 'should return a string', function() {
 			const value = rhythm.lines( '1' );
 			assert.typeOf( value, 'string' );
+		} );
+
+		it( 'should return the correct value', function() {
+			const value = rhythm.lines( '1' );
+			assert.equal( value, '1rem' );
+		} );
+
+		it( 'should return a lower value if subtractPx is defined', function() {
+			const valueToBeBelow = parseInt( rhythm.lines( '1' ), 10 );
+			const valueToCheck   = parseInt( rhythm.lines( '1', '1px' ), 10 );
+
+			assert.isBelow( valueToCheck, valueToBeBelow );
+		} );
+
+		it( 'should be appended with the rem unit', function() {
+			const value = rhythm.lines( '1' );
+			assert.match( value, /rem$/ );
 		} );
 	} );
 } );
